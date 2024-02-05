4f4e5d4f3b079cf699395c1be14b3cd3702a389e
Anshu Harshit <114798033+anshu-harshit01@users.noreply.github.com>
2024-02-05 05:44:40 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version.
+
+//alert(10+20)//we are using node js not browser.
+
+let name ="Anshu Harshit"
+let val = 19
+let isLoggedIn = false
+let state
+
+//number => 2^53. 
+//Bigint
+//string - can use in single quote & double quote.
+//boolean = true / false
+//null= => standalone value
+//undefined - means abhi value asign nhi hui hai.
+// symbol - unique
+
+//object
+// a/c to JS null is a typeof object.
+console.log(typeof name)
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
