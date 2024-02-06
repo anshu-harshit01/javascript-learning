5223b4185ac4b8035aedcfe8c86beb84268dad47
Anshu Harshit <114798033+anshu-harshit01@users.noreply.github.com>
2024-02-06 09:20:29 +0000

    Datatypes Conversion

---
+++ 01-basics/conversionOperation.js
@@ -0,0 +1,51 @@
+//  let adam = 24
+//  console.log(typeof adam);//print statement
+//  console.log(typeof(adam));
+
+//  let ravi = "24"
+//  console.log(typeof ravi);
+
+//  let akash = "31"
+//  let convertedAkash = Number(akash);
+//  console.log(typeof convertedAkash);
+
+//  let ansu = "33aaa"
+//  console.log(typeof ansu);
+//  let reverseAnsu = Number(ansu)
+//  console.log(typeof reverseAnsu);
+//  console.log(reverseAnsu);
+
+// let ansu = null
+// console.log(typeof ansu);
+// let reverseAnsu = Number(ansu)
+// console.log(typeof reverseAnsu);
+// console.log(reverseAnsu);
+
+// let ansu = undefined
+// console.log(typeof ansu);
+// let reverseAnsu = Number(ansu)
+// console.log(typeof reverseAnsu);
+// console.log(reverseAnsu);
+
+// let ansu = false
+// console.log(typeof ansu);
+// let reverseAnsu = Number(ansu)
+// console.log(typeof reverseAnsu);
+// console.log(reverseAnsu);
+ // "33" => 33
+ // "33and" => NaN
+ //true => 1; false => 0 
+
+//  let tryBool = 0
+//  let convertInBool = Boolean(tryBool)
+//  console.log(convertInBool);
+
+// let tryEmpty = "Anshu"
+// let convert = Boolean(tryEmpty)
+// console.log(typeof convert);
+// console.log(convert);
+
+let num = 2
+let inString = String(num)
+console.log(typeof(inString));
+console.log(inString);
