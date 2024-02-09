4dc29273ff560139d2fcd31cc571fdd4220ebba1
Anshu Harshit <114798033+anshu-harshit01@users.noreply.github.com>
2024-02-09 04:07:07 +0000

    pending

---
+++ 01-basics/datatypesSummary.js
@@ -0,0 +1,18 @@
+// Primitive datatypes
+// These are call by value.
+//7 types
+//String, number, null, undefined, Symbol, Boolean, BigInt..
+
+const score = anshu
+const num = 12
+const isvalid = false
+const value = null // null means empty, not zero!
+let userEmail; //is varaible me jo value hogi wo undefined hogi.
+const id = Symbol('18275')
+const otherId = Symbol('18275')
+console.log(id===otherId);
+console.log(id);
+console.log(otherId);
+
+//Non-Primitive (call by reference)
+//Array, Objects, Functions.
