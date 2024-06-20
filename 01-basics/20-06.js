c1503778503555960ab4ccf4b2f65db6a35f5e86
Anshu Harshit <114798033+anshu-harshit01@users.noreply.github.com>
2024-06-20 11:59:25 +0000

    container config done

---
+++ 01-basics/20-06.js
@@ -0,0 +1,25 @@
+// console.log("anshu");
+// const num1 = 12;
+// var num2 = 14;
+// console.log(num1+num2);
+// num1 = 20;
+// console.log(num1+num2);
+// console.log(z);
+// var z = 12;
+// var arr=[12, 13, 14];
+// var b = arr;
+// b.pop();
+// console.log(b);
+// console.log(arr);
+// if("anshu"){
+//     console.log("in if");
+// }
+var a = 1;
+for(var i=0; i<10; i++)
+{
+    console.log(a++);
+}
+while(a != 10)
+{
+    console.log(a++);
+}
